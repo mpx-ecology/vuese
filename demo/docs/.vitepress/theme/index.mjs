2527525d274de69f5c1819e0d076bafbf90a30f0
QiuShuiBai <1125097005@qq.com>
2024-06-24 14:43:40 +0800

    feat: 添加card组件

--- demo/docs/.vitepress/theme/index.mjs
+++ demo/docs/.vitepress/theme/index.mjs
@@ -1,10 +1,12 @@
 import VueseWebsiteTheme from '@mpxjs/vuese-website/dist/theme/index'
 import Layout from '@mpxjs/vuese-website/dist/theme/layouts/Layout.vue'
+import Card from '@mpxjs/vuese-website/dist/theme/global-components/Card.vue'
 
 export default {
   extend: VueseWebsiteTheme,
   Layout: Layout,
-  enhanceApp({ router }) {
-    router.go('/mpx-ui/guide/button.html')
+  enhanceApp({ router, app }) {
+    router.go('/mpx-ui/guide/button.html'),
+    app.component('Card', Card)
   }
 }
